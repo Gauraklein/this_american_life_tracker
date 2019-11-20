31418581c137f9eff614a4cab83900c0c9d30f91
gauraklein <gaurakklein@gmail.com>
2019-11-19 22:02:41 -0600

    cleaned up root, react is working currently only have a login page running but I can now begin building the full app

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Component } from 'react'
-import logo from './logo.svg';
+import logo from './talLogo.png';
 import './App.css';
 
 class App extends Component {
@@ -23,11 +23,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to the front/backend</h1>
-        </header>
-    <p className="App-intro">{this.state.apiResponse}</p>
+          <Nav />
+          <LoginBody />
+          <div className="centered flex2">
+            <p className="App-intro">{this.state.apiResponse}</p>
+          </div>
+          <Footer />
       </div>
     )
   }
@@ -35,3 +36,74 @@ class App extends Component {
 }
 
 export default App;
+
+const Nav = (props) => {
+  return (
+    <nav className="flex1 centered flex-row">
+
+      <div className="nav-left flex1 centered flex-column">
+          <img className="navLogo"  src={logo} alt="This American life logo" />
+      </div>
+
+    <div class="flex8 navSpacer"></div>
+
+    <div class="flex1 nav-right centered flex-row">
+        <ul className="navMenu">
+            <li><a href="#">Login</a></li>
+            <li><a href="#about">About</a></li>
+        </ul>
+    </div>
+  </nav>
+  )
+}
+
+const LoginBody = (props) => {
+
+  return (
+    <div className="flex4 title flex-column centered">
+              <h3 className="flex2">The Best Way to Keep Track of Your Listening History</h3>
+    
+               <div  className="loginForm flex6 flex-column">
+                    <form action="submit">
+                           UserName:<input type="text" />
+                           Password:<input type="text"/>
+                    </form>
+                </div>
+                
+                <div class="flex2" id="about">
+                <p>This project started by filling a need to keep track of which episodes of 'This American Life' I have listened to
+                     as well as a way to keep track of my ratings of those episodes.
+                 </p>
+             </div>
+     </div> 
+  )
+}
+
+
+
+const Footer = (props) => {
+
+  return (
+    <div id="footer" className="centered flex1">
+        <p>Footer Goes Here</p>
+    </div>
+  )
+}
+
+{/* // const Login = (props) => { */}
+{/* //   return (
+ 
+
+
+//    
+
+//             <div class="flex2" id="about">
+//                 <p>This project started by filling a need to keep track of which episodes of 'This American Life' I have listened to
+//                     as well as a way to keep track of my ratings of those episodes.
+//                 </p>
+//             </div>
+//     </div> }
+
+{/* //     
+//   )
+// } */}
