51a9df94abbf9e59a979915cddfb615893ee96de
gauraklein <gaurakklein@gmail.com>
2019-11-23 17:29:57 -0600

    working on authentication

---
+++ migrations/20191123170224_users.js
@@ -0,0 +1,13 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('users', (table) => {
+        table.integer('id')
+        table.string('user_email')
+        table.string('user_password')
+
+    })
+};
+
+exports.down = function(knex) {
+    return knex.schema.raw('DROP TABLE users')
+};
--- server.js
+++ server.js
@@ -5,11 +5,76 @@ const express = require('express')
 var cors = require('cors');
 const app = express()
 const { db } = require("./db")
-const stringify = require('json-stringify-safe')
+const session = require("express-session")
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcrypt");
+
+
+
 
 
 const port = 9000
 
+
+//--------------------------------------\\
+//               PASSPORT               \\
+//--------------------------------------\\
+
+passport.use(
+    new LocalStrategy((email, password, done) => {
+      console.log("got auth request");
+      db("users")
+        .where({ email: email })
+        // .orwhere({ email: username })
+        .then(res => {
+          // console.log(userRows)
+          const user = res[0];
+          // console.log(user);
+          if (!user) {
+            console.log("User not found");
+            done(null, false);
+          } 
+          
+          if (bcrypt.compareSync(user.password, password)) {
+            console.log("Wrong Password");
+            done(null, false);
+          } else
+          console.log("User found");
+          return done(null, user);
+        })
+        .catch(err => {
+          console.error("Local strategy error - ", err);
+          return err;
+        });
+    })
+  );
+  app.use(
+    session({
+      secret: "keyboard cat",
+      resave: false,
+      saveUninitialized: true,
+      cookie: {}
+    })
+  );
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(express.static("public"));
+  passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+  passport.deserializeUser(function(id, done) {
+    db("users")
+      .where({ id: id })
+      .then(res => {
+        done(null, res[0]);
+      })
+      .catch(error => done(error, false));
+  });
+  
 
 app.use(cors())
 app.use(express.static(path.join(__dirname, 'public')));
@@ -23,6 +88,10 @@ app.use(express.static(path.join(__dirname, 'public')));
     } )
     
 });
+
+app.get('/login', function(req, res, next) {
+    res.sendFile(path.join(__dirname + '/login.html'));
+})
 
 const getEpisodes = () => {
     return db.raw(
