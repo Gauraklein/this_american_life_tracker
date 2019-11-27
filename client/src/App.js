c6b565d440380b2fdad06c5f5980445e740cb56e
gauraklein <gaurakklein@gmail.com>
2019-11-26 20:29:14 -0600

    organized src by adding modules

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Component } from 'react'
-import logo from './talLogo.png';
 import './App.css';
-import Fade from 'react-reveal/Fade'
 
-const moment = require("moment")
+// COMPONENTS 
+import { EpisodeContainer } from './Components/EpisodeContainer'
+import { Nav } from './Components/Nav'
+import { Footer } from './Components/Footer'
+
 
 class App extends Component {
   constructor(props) {
@@ -42,117 +44,13 @@ class App extends Component {
 
 export default App;
 
-const EpisodeContainer = ({episodes}) => {
-  
-  return (
-    
-    <div className="episodeCardContainer centered flex8 flex-wrap flex-row">
-    {episodes.map(renderEpisodeCard)}
-  </div>
-  )
-}
-
-const renderEpisodeCard = (episodeMetadata) => {
-
-  let backgroundImage = episodeMetadata.image
-
-  if (backgroundImage == null) {
-    backgroundImage = logo
-  }
-
-  let publishDate = moment(episodeMetadata.date_published).format('MMM Do YYYY')
-  
-  let wrapperStyle = {
-    backgroundImage: "url(" + backgroundImage + ")",
-    backgroundSize: "cover",
-    backgroundPosition: "center"
-  };
-
-  let fadeDuration = Math.floor(Math.random() * 2001)
-
-  return (
-    <Fade big cascade duration={fadeDuration}>
-    <div className="episodeCardStyle card">
-    <div className="wrapper" style={wrapperStyle}>
-      <div className="date">
-        {publishDate}
-      </div>
-      <div className="data">
-        <div className="content">
-        <div className="readMoreFooter">
-        <span className="author">Episode {episodeMetadata.episode_number}</span>
-
-      <a href="#" className="button">Read more</a>
-
-      </div>
-          <h1 className="title"><a href="#">{episodeMetadata.episode_title}</a></h1>
-          <p className="text">{episodeMetadata.episode_description}</p>
-        
-        </div>
-      </div>
-
-      
-  </div>
-  </div>
-
-  </Fade>
-
-  )
-
-}
 
 
 
-const Nav = (props) => {
-  return (
-    <nav className="navBar flex1 flex-row">
-
-      <div className="nav-left flex1 centered flex-column">
-          <img className="navLogo"  src={logo} alt="This American life logo" />
-      </div>
-
-    <div className="flex8 navSpacer"></div>
-
-    <div className="flex1 nav-right centered flex-row">
-        <ul className="navMenu">
-            <li><a href="#">Login</a></li>
-            <li><a href="#about">About</a></li>
-        </ul>
-    </div>
-  </nav>
-  )
-}
-
-const LoginBody = (props) => {
-
-  return (
-    <div className="flex4 title flex-column centered">
-              <h3 className="flex2">The Best Way to Keep Track of Your Listening History</h3>
-    
-               <div  className="loginForm flex6 flex-column">
-                    <form action="submit">
-                           UserName:<input type="text" />
-                           Password:<input type="text"/>
-                    </form>
-                </div>
-                
-                <div className="flex2" id="about">
-                <p>This project started by filling a need to keep track of which episodes of 'This American Life' I have listened to
-                     as well as a way to keep track of my ratings of those episodes.
-                 </p>
-             </div>
-     </div> 
-  )
-}
 
 
 
-const Footer = (props) => {
 
-  return (
-    <div id="footer" className="centered flex1">
-        <p>Footer Goes Here</p>
-    </div>
-  )
-}
+
+
 
